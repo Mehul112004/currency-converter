fd780efb2470d5954d283552a223b914cce490a8
Mehul <mehulbohra11@gmail.com>
2023-10-25 18:34:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [to, setTo] = useState("inr");
   const [convertedAmount, setConvertedAmount] = useState(null);
 // console.log(from);
-  const BackgroundImage="https://images.unsplash.com/photo-1691273553489-615e4279af43?auto=format&fit=crop&q=80&w=870&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  const BackgroundImage="https://images.pexels.com/photos/1144230/pexels-photo-1144230.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 
   const CurrencyInfo = useCurrencyInfo(from);
 
